joelmeans/pymetadatamanager

commit bdf4d971a6c13c490a773b9518f2369d64371c93

Fixed up a few errors in the GUI code.

pymetadatamanager/MainWindow.py
@@ -93,6 +93,12 @@ class MainWindow(QtGui.QMainWindow):
         self.episode_tvdb_id_updated = 0
         self.episode_tvdb_rating_updated = 0
 
+        #Create some empty lists for later
+        self.series_banners_url = []
+        self.series_banners_wide_url = []
+        self.series_fanart_banners_url = []
+        self.season_banners_url = []
+        self.season_banners_wide_url = []
 
     def column_view_clicked(self, index):
         """Determines what was clicked in the column view tree"""
@@ -308,9 +314,6 @@ class MainWindow(QtGui.QMainWindow):
         series_root = series_doc.firstChildElement('tvshow')
         #These are the lists we will populate from the series info
         season_banners_url = []
-        self.series_banners_url = []
-        self.series_banners_wide_url = []
-        self.series_fanart_banners_url = []
         elem_series_banner = series_root.firstChildElement('thumb')
         while not elem_series_banner.isNull():
             try:
@@ -358,8 +361,6 @@ class MainWindow(QtGui.QMainWindow):
         series_doc = dbTV.make_series_dom(series_id)
         series_root = series_doc.firstChildElement('tvshow')
         #These are the lists we will populate from the season info
-        self.season_banners_url = []
-        self.season_banners_wide_url = []
         elem_series_banner = series_root.firstChildElement('thumb')
         while not elem_series_banner.isNull():
             try:

pymetadatamanager/mediainfo.py
@@ -22,10 +22,8 @@ __author__="jmeans"
 __date__ ="$Feb 12, 2010 9:26:59 AM$"
 
 import os
-import re
 from subprocess import Popen
 from PyQt4 import QtXml, QtCore
-from BeautifulSoup import BeautifulSoup
 
 class MediaInfo(object):
     """
@@ -48,8 +46,11 @@ class MediaInfo(object):
         self.temp_file = self.temp_dir + "/info.xml"
 
     def __del__(self):
-        if os.path.exists(self.temp_file):
-            os.remove(self.temp_file)
+        try:
+	    if os.path.exists(self.temp_file):
+                os.remove(self.temp_file)
+        except AttributeError:
+	    pass
         
     def make_info_xml_native(self, infile, outfile="none"):
         """Calls 'mediainfo' to output an xml file with metadata for infile"""

pymetadatamanager/models.py
@@ -181,7 +181,10 @@ class AbstractBannerModel(QtCore.QAbstractTableModel):
          self.data[index.row()][index.column()])
 
     def columnCount(self, parent):
-        return len(self.data[0])
+	try:    
+            return len(self.data[0])
+	except IndexError:
+	    return 0
 
     def rowCount(self, parent):
         return len(self.data)

pymetadatamanager/scraper_ui.py
@@ -181,7 +181,7 @@ class Ui_MainWindow(object):
 
         #Add the tab for series posters (banners)
         self.tab_series_banners = QtGui.QWidget()
-        self.tab_series_banners.setObjectName("tab_series_bannerss")
+        self.tab_series_banners.setObjectName("tab_series_banners")
 
         #A table view for the series posters on the tab
         self.tableView_series_banners = QtGui.QTableView(self.tab_series_banners)

commit d8a407ef00fe2b75904dd6d9584003c58723e908

Fixed a bug introduced in bdf4d97...

pymetadatamanager/MainWindow.py
@@ -314,6 +314,9 @@ class MainWindow(QtGui.QMainWindow):
         series_root = series_doc.firstChildElement('tvshow')
         #These are the lists we will populate from the series info
         season_banners_url = []
+	self.series_banners_url = []
+        self.series_banners_wide_url = []
+	self.series_fanart_banners_url = []
         elem_series_banner = series_root.firstChildElement('thumb')
         while not elem_series_banner.isNull():
             try:
@@ -361,6 +364,8 @@ class MainWindow(QtGui.QMainWindow):
         series_doc = dbTV.make_series_dom(series_id)
         series_root = series_doc.firstChildElement('tvshow')
         #These are the lists we will populate from the season info
+        self.season_banners_url = []
+	self.season_banners_wide_url = []
         elem_series_banner = series_root.firstChildElement('thumb')
         while not elem_series_banner.isNull():
             try: